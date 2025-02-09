34f82bb26a12911b01931c412f5038d3b24e9711
michelebswm <michelebsant@gmail.com>
2025-02-08 22:09:33 -0300

    feat: adiciona findByUsername

--- src/main/java/br/com/madr/repository/UserRepository.java
+++ src/main/java/br/com/madr/repository/UserRepository.java
@@ -4,6 +4,11 @@ import br.com.madr.domain.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Optional;
+
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
+    public Optional<User> findByUsername(String username){
+        return find("username", username).firstResultOptional();
+    }
 }
