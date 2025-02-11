cff24371ea555c0b557df731f275628cb0d07504
michelebswm <michelebsant@gmail.com>
2025-02-11 20:16:21 -0300

    feat: adiciona as validações NotNull e Size aos fields

--- src/main/java/br/com/madr/domain/User.java
+++ src/main/java/br/com/madr/domain/User.java
@@ -1,6 +1,8 @@
 package br.com.madr.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
 
@@ -22,12 +24,16 @@ public class User {
     private Long id;
 
     @Column(name = "username", nullable = false, length = 50)
+    @NotNull(message = "{user.username.notnull}")
     private String username;
 
     @Column(name = "email", nullable = false, length = 150)
+    @NotNull(message = "{user.email.notnull}")
     private String email;
 
     @Column(name = "senha", nullable = false, length = 128)
+    @NotNull(message = "{user.senha.notnull}")
+    @Size(min = 6, max = 128, message = "{user.senha.size}")
     private String senha;
 
     public User() {
