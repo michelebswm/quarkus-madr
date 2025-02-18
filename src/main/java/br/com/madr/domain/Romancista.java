90e62acc25e4cac3e39f8533e0bd50361d26aa43
michelebswm <michelebsant@gmail.com>
2025-02-17 21:11:48 -0300

    feat: adiciona validação de Notnull e Size

--- src/main/java/br/com/madr/domain/Romancista.java
+++ src/main/java/br/com/madr/domain/Romancista.java
@@ -2,6 +2,8 @@ package br.com.madr.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
 import java.util.Set;
@@ -23,6 +25,8 @@ public class Romancista {
     private Long id;
 
     @Column(name = "nome", nullable = false, length = 500)
+    @NotNull(message = "{romancista.nome.notnull}")
+    @Size(min = 5, max = 500, message = "{romancista.nome.size}")
     private String nome;
 
     @JsonIgnore
@@ -73,5 +77,14 @@ public class Romancista {
     @Override
     public int hashCode() {
         return Objects.hash(id, nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Romancista{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", livros=" + livros +
+                '}';
     }
 }
