e3831c4fbf0399d604822a7a033ffb43bcb40ad7
michelebswm <michelebsant@gmail.com>
2025-02-17 21:12:09 -0300

    feat: adiciona metodo findByNome

--- src/main/java/br/com/madr/repository/RomancistaRepository.java
+++ src/main/java/br/com/madr/repository/RomancistaRepository.java
@@ -1,9 +1,16 @@
 package br.com.madr.repository;
 
 import br.com.madr.domain.Romancista;
+import br.com.madr.domain.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Optional;
+
 @ApplicationScoped
 public class RomancistaRepository implements PanacheRepository<Romancista> {
+
+    public Optional<Romancista> findByNome(String nome){
+        return find("nome", nome).firstResultOptional();
+    }
 }
