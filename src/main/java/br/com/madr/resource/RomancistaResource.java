aed39b57d42e5aab081ce5bb934b66cb537e8e41
michelebswm <michelebsant@gmail.com>
2025-02-18 22:02:14 -0300

    feat: adiciona os metodos getById , patch e deleteById

--- src/main/java/br/com/madr/resource/RomancistaResource.java
+++ src/main/java/br/com/madr/resource/RomancistaResource.java
@@ -3,13 +3,13 @@ package br.com.madr.resource;
 import br.com.madr.dto.response.RomancistaDTOResponse;
 import br.com.madr.exception.ApplicationServiceException;
 import br.com.madr.service.RomancistaService;
+import br.com.madr.utils.message.MessageResponse;
+import br.com.madr.utils.message.MessageService;
 import br.com.madr.vo.RomancistaVO;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import org.jboss.resteasy.reactive.RestResponse;
 
@@ -24,6 +24,17 @@ public class RomancistaResource {
         this.romancistaService = romancistaService;
     }
 
+    @GET
+    @Path("/{id}")
+    public RestResponse<RomancistaDTOResponse> getById(@PathParam("id") Long id) throws ApplicationServiceException {
+        RomancistaDTOResponse response = this.romancistaService.getById(id);
+        return RestResponse.ResponseBuilder
+                .<RomancistaDTOResponse>ok()
+                .entity(response)
+                .build();
+
+    }
+
 
     @POST
     public RestResponse<RomancistaDTOResponse> register(RomancistaVO romancistaVO, @Context UriInfo uriInfo) throws ApplicationServiceException {
@@ -33,5 +44,19 @@ public class RomancistaResource {
                 .entity(response)
                 .build();
 
+    }
+
+    @PATCH
+    @Path("/{id}")
+    public RestResponse<Void> updatedPatch(@PathParam("id") Long id, RomancistaVO romancistaVO) throws ApplicationServiceException {
+        this.romancistaService.updatePatch(id, romancistaVO);
+        return RestResponse.noContent();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public RestResponse<MessageResponse> deleteById(@PathParam("id") Long id) throws ApplicationServiceException {
+        this.romancistaService.deleteById(id);
+        return RestResponse.ok(new MessageResponse("Romancista deletada no MADR"));
     }
 }
--- src/main/java/br/com/madr/service/RomancistaService.java
+++ src/main/java/br/com/madr/service/RomancistaService.java
@@ -30,6 +30,19 @@ public class RomancistaService {
         this.romancistaRepository = romancistaRepository;
     }
 
+    public RomancistaDTOResponse getById(Long id) throws ApplicationServiceException {
+
+        Romancista romancista = this.romancistaRepository.findById(id);
+
+        if (romancista == null){
+            LOG.info("Debug na execucao do RomancistaService: findById = não existe - id="+id);
+            throw new ApplicationServiceException("romancista.naocadastrado", Response.Status.NOT_FOUND.getStatusCode());
+        }
+        
+        return RomancistaDTOResponse.fromEntity(romancista);
+
+    }
+
 
     @Transactional
     public RomancistaDTOResponse register(RomancistaVO romancistaVO) throws ApplicationServiceException {
@@ -54,6 +67,59 @@ public class RomancistaService {
         } catch (Exception e) {
             LOG.log(Level.SEVERE, "Error in the execution of RomancistaService: register");
             throw new ApplicationServiceException("romancista.erro", new String[]{"register"});
+        }
+    }
+
+    @Transactional
+    public void updatePatch(Long id, RomancistaVO romancistaVO) throws ApplicationServiceException {
+        LOG.log(Level.INFO, "RomancistaService : updatePatch");
+        Romancista romancista = this.romancistaRepository.findById(id);
+
+        if (romancista == null){
+            throw new ApplicationServiceException("romancista.naocadastrado", Response.Status.NOT_FOUND.getStatusCode());
+        }
+
+        try{
+            if (romancistaVO.getNome() != null){
+                romancista.setNome(romancistaVO.getNome());
+            }
+            this.romancistaRepository.persistAndFlush(romancista);
+        }catch (ConstraintViolationException cve){
+            if (cve.getConstraintName() == null && cve.getSQLException() != null) {
+                String sqlMessage = cve.getSQLException().getMessage();
+                if (sqlMessage.contains("uk_nome_romancista")) {
+                    throw new ApplicationServiceException("romancista.uk_nome_romancista");
+                }
+            }
+            throw new ApplicationServiceException("romancista." + cve.getConstraintName());
+        }catch (jakarta.validation.ConstraintViolationException cve) {
+            System.out.println("cve: " + cve.getMessage());
+            throw new ApplicationServiceException("message.parametrosnaoinformados",
+                    Response.Status.BAD_REQUEST.getStatusCode(),
+                    new MessageService(cve).getErrors()
+            );
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Error in the execution of RomancistaService: updatePatch");
+            throw new ApplicationServiceException("romancista.erro", new String[]{"updatePatch"});
+        }
+    }
+
+    @Transactional
+    public void deleteById(Long id) throws ApplicationServiceException {
+        LOG.log(Level.INFO, "RomancistaService: deleteById");
+
+        Romancista romancista = this.romancistaRepository.findById(id);
+        if (romancista == null){
+            LOG.info("Debug na execucao do RomancistaService: deleteById = não existe - id="+id);
+            throw new ApplicationServiceException("romancista.naocadastrado", Response.Status.NOT_FOUND.getStatusCode());
+        }
+
+        try{
+            this.romancistaRepository.deleteById(id);
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE,"Erro na execucao do RomancistaService: deleteById", e);
+            throw new ApplicationServiceException("romancista.erro", new String[] { "deleteById" },
+                    Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
         }
     }
 
