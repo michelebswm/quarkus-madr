709e42719aa9e65fde1f17b141c94301f33d9216
michelebswm <michelebsant@gmail.com>
2025-02-18 22:01:21 -0300

    feat: adiciona no repository o validAndMerge

--- src/main/java/br/com/madr/repository/RomancistaRepository.java
+++ src/main/java/br/com/madr/repository/RomancistaRepository.java
@@ -1,9 +1,9 @@
 package br.com.madr.repository;
 
 import br.com.madr.domain.Romancista;
-import br.com.madr.domain.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.validation.Valid;
 
 import java.util.Optional;
 
@@ -12,5 +12,9 @@ public class RomancistaRepository implements PanacheRepository<Romancista> {
 
     public Optional<Romancista> findByNome(String nome){
         return find("nome", nome).firstResultOptional();
+    }
+
+    public void validAndMerge(@Valid Romancista romancista) {
+        getEntityManager().merge(romancista);
     }
 }
