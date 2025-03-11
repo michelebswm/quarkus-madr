a98689dc90f4a71dc4e613fd3232403edbdf9629
michelebswm <michelebsant@gmail.com>
2025-03-11 20:34:51 -0300

    feat: cria um metodo findByEmail

--- src/main/java/br/com/madr/repository/UserRepository.java
+++ src/main/java/br/com/madr/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.madr.repository;
 
 import br.com.madr.domain.User;
+import br.com.madr.exception.ApplicationServiceException;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.core.Response;
 
 import java.util.Optional;
 
@@ -11,6 +13,12 @@ import java.util.Optional;
 public class UserRepository implements PanacheRepository<User> {
     public Optional<User> findByUsername(String username){
         return find("username", username).firstResultOptional();
+    }
+
+    public User findByEmail(String email) throws ApplicationServiceException{
+        return find("email", email).firstResultOptional().orElseThrow(
+            () -> new ApplicationServiceException("message.forbidden", Response.Status.FORBIDDEN.getStatusCode())
+        );
     }
 
     public void validAndMerge(@Valid User user) {
